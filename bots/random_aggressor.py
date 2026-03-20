2191511853bfe0144e2cf051e304d7603e192de7
Juan Pedro Fisanotti <fisadev@gmail.com>
2026-03-20 20:33:00 -0300

    A couple of silly bots to use as demo

---
+++ bots/random_aggressor.py
@@ -0,0 +1,27 @@
+import random
+
+from tv.game import positions_around
+
+
+class BotLogic:
+    """
+    A bot that just moves randomly trying to hurt enemies, doesn't care about anything else.
+    """
+    def initialize(self):
+        """
+        This bot doesn't need to initialize anything.
+        """
+        pass
+
+    def turn(self, hp, cargo, position, power_distribution, radar_contacts):
+        """
+        This bot sets up power to the lasers and just moves randomly, expecting to hurt other ships
+        in the process.
+        """
+        desired_distribution = {"engines": 1, "shields": 0, "lasers": 2}
+
+        if power_distribution != desired_distribution:
+            return "power_to", desired_distribution
+        else:
+            # move to a random destination
+            return "fly_to", random.choice(list(positions_around(position, 1)))
---
+++ bots/randomaniac.py
@@ -0,0 +1,34 @@
+import random
+
+from tv.game import positions_around, MAX_POWER
+
+
+class BotLogic:
+    """
+    A bot that just moves randomly and reconfigures the spaceship randomly.
+    """
+    def initialize(self):
+        """
+        This bot doesn't need to initialize anything.
+        """
+        pass
+
+    def turn(self, hp, cargo, position, power_distribution, radar_contacts):
+        """
+        This bot chooses its actions completely at random.
+        """
+        if random.random() < 0.8:
+            # move to a random destination
+            speed = power_distribution["engines"]
+            possible_destinations = list(positions_around(position, speed))
+            destination = random.choice(possible_destinations)
+
+            return "fly_to", destination
+        else:
+            # randomly distribute power
+            power_distribution = {"engines": 0, "shields": 0, "lasers": 0}
+            for _ in range(MAX_POWER):
+                system = random.choice(["engines", "shields", "lasers"])
+                power_distribution[system] += 1
+
+            return "power_to", power_distribution
